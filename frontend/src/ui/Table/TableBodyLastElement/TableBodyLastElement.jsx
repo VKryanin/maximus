c237f4a2c49c8770661c0108a609b9bf41cddd5c
Vitaly Kryanin <krianinvv@internet.ru>
2024-06-08 19:54:26 +0300

    add gitignore

--- frontend/src/ui/Table/TableBodyLastElement/TableBodyLastElement.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const TableBodyLastElement = styled.tr`
-  display: block;
-  position: relative;
-  bottom: 100px;
-`;
-
-export default TableBodyLastElement;
